c83f8cf02fc918b7c1870c6f763d59466d6bea9f
Can Ozdal <canozdal@hotmail.com>
2022-12-20 22:20:29 +0200

    Add Card and Address
    AddCard and AddNewAddress are writing new entries to the database

--- src/components/AddNewAddress.js
+++ src/components/AddNewAddress.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import  Axios  from "axios";
 
 export const AddNewAddress = () =>{
     const navigate = useNavigate();
+
+    const [address,setAddress] = useState()
+    const [email,setEmail] = useState()
+
+    const addNewAddress = () =>{
+        Axios.post('http://localhost:3005/addshippingaddress',{email: email,address: address})
+        .then(() => {
+            console.log("success")
+        })
+    }
     return(
         <div align="center">
              <p>Add New Address</p>
              <p>Full Name</p>
              <input></input>
              <p>Email</p>
-             <input></input>
+             <input onChange={(e) => setEmail(e.target.value)}></input>
              <p>Phone Number</p>
              <input></input>
              <p>Address</p>
-             <input></input>
+             <input onChange={(e) => setAddress(e.target.value)}></input>
              <p>City</p>
              <input></input>
              <p>Post Code</p>
              <input></input>
-             <button onClick={() => navigate(-1)}>Add</button>
+             <button onClick={() => {
+                addNewAddress();
+                navigate(-1)}}>Add</button>
              <button onClick={() => navigate(-1)}>Back</button>
              </div>
 
--- src/components/AddNewCard.js
+++ src/components/AddNewCard.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import  Axios  from "axios";
 export const AddNewCard = () => {
     const navigate = useNavigate();
     const [cardType,setCardType] = useState();
+    const [name,setName] = useState();
+    const [exp_date,setExpDate] = useState();
+    const [cvv,setCvv] = useState();
+    const [card_no,setCardNo] = useState();
+
+    const addNewCard = () =>{
+        Axios.post('http://localhost:3005/addcreditcard',{name: name,cvv: cvv,exp_date: exp_date,card_no: card_no}).then(
+            ()=>{
+                console.log("Succesful");
+            }
+        )
+    }
     return(
         <div align="center">
             <p> Card Type</p>
@@ -12,15 +24,17 @@ export const AddNewCard = () => {
             <input type="radio" name="cardType" value="Paypal" onChange={e=>setCardType(e.target.value)}/>Paypal
             <br></br>
             <p>Name on the card</p>
-            <input></input>
+            <input type="text" onChange={(e)=>setName(e.target.value)}></input>
             <p>Card Number</p>
-            <input></input>
+            <input onChange={(e)=>setCardNo(e.target.value)}></input>
             <p>Expiration Date</p>
-            <input></input>
+            <input onChange={(e)=> setExpDate(e.target.value)}></input>
             <p>CVV</p>
-            <input></input>
+            <input onChange={(e)=>setCvv(e.target.value)}></input>
             <br></br>
-            <button onClick={()=>navigate(-1)}>Add</button>
+            <button onClick={()=>{
+                addNewCard();
+                navigate(-1)}}>Add</button>
             <button onClick={()=>navigate(-1)}>Back</button>
         </div>
     );
